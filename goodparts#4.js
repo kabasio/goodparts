f5ec2eb3eb6bf29c4bc2e210e295d4ba12489c17
kabashima shiori <sor.0810@gmail.com>
2017-09-13 23:40:23 +0900

    learnd about Arrow function

--- goodparts#4.js
+++ goodparts#4.js
@@ -20,7 +20,7 @@ console.log(myObject.value); // 1
 
 const myObject = {
   value: 0,
-  increment: (inc) => {
+  increment(inc) {
     this.value += typeof inc === 'number' ? inc : 1;
     console.log(this.value);
   },
@@ -29,3 +29,28 @@ const myObject = {
 myObject.increment();
 
 console.log(myObject.value);// 1
+
+const sum = add(3, 4);
+
+console.log(sum); // 7
+
+myObject.double = function () {
+//  const that = this;
+
+  const helper = () => {
+    this.value = add(this.value, this.value);
+  };
+
+  helper();
+};
+
+// helperは単品で実行されてるので、thisはグローバルオブジェクトを見に行ってしまう。
+// それを回避するのがアロー関数
+
+console.log('before double');
+console.log(myObject.value);
+myObject.double();
+// result undefined
+console.log('after double');
+console.log(myObject.value);
+
